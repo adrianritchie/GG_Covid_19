094aff7e9c0cfeaedda229015ff8981d725831b9
Adrian RItchie <ar@kodo.gg>
2020-03-26 21:51:49 +0000

    Auto stash before merge of "master" and "origin/master"

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,9 +5,14 @@ import * as cheerio from 'cheerio';
 import * as moment from 'moment';
 
 admin.initializeApp();
+const db = admin.firestore();
+
+const url = 'https://www.gov.gg/covid19testresults';
 
 let results: any;
 let lastGrabbed: any = null;
+
+let latest: any;
 
 const storeUpdate = function (data: any, type: string) {
     if (lastGrabbed < new Date(data['Updated'])) {
@@ -23,13 +28,13 @@ const storeUpdate = function (data: any, type: string) {
             }
         }
 
-        admin.firestore().collection('tracking').add(data).catch((err) => { console.log("Error: " + err.message); });
-        admin.firestore().collection('web_hooks').limit(1).get()
+        db.collection('tracking').add(data).catch((err) => { console.log("Error: " + err.message); });
+        db.collection('web_hooks').limit(1).get()
             .then(querySnapshot => {
                 querySnapshot.forEach(documentSnapshot => {
-                    for (const url of documentSnapshot.get('urls')) {
-                        console.log("Webhook url: " + url);
-                        https.get(url);
+                    for (const webHook of documentSnapshot.get('urls')) {
+                        console.log("Webhook url: " + webHook);
+                        https.get(webHook);
                     }
                 });
             })
@@ -46,8 +51,58 @@ const storeUpdate = function (data: any, type: string) {
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const refreshData = functions.https.onRequest((request, response) => {
-    const url = 'https://www.gov.gg/covid19testresults';
+export const refreshData = functions.region('europe-west2').https.onRequest((request, response) => {
+    const req = https.get(url, (resp) => {
+        let data: string = '';
+        resp.on('data', (chunk) => { data += chunk; });
+        resp.on('end', () => {
+            const $ = cheerio.load(data);
+            const date_updated = $('meta[name="DC.date.modified"]').attr('content');
+            const table = $('table').first();
+
+            const scraped_data: Array<Array<string>> = [];
+
+            table.find('tr').map(function (i, el) {
+                scraped_data.push([]);
+                return $(this).find('td').map(function (j, fl) {
+                    scraped_data[i].push($(this).text())
+                })
+            });
+
+            const o: any = {};
+            for (let i = 0; i < scraped_data[0].length; i++) {
+                o[scraped_data[0][i]] = parseInt(scraped_data[1][i]);
+            }
+            o["Updated"] = date_updated;
+            o["Saved"] = new Date().toISOString();
+
+            if (!!lastGrabbed) {
+                const result = storeUpdate(o, 'quick check')
+                response.send(result);
+            }
+            else {
+                db.collection('tracking')
+                    .orderBy('Updated', 'desc').limit(1).get()
+                    .then(querySnapshot => {
+                        querySnapshot.forEach(documentSnapshot => {
+                            lastGrabbed = new Date(documentSnapshot.get('Updated'));
+                        });
+                    })
+                    .then(() => {
+                        const result = storeUpdate(o, 'reload check')
+                        response.send(result);
+                    })
+                    .catch(err => response.send(err));
+            }
+        });
+
+    }).on("error", (err) => { console.log("Error: " + err.message); });
+
+
+});
+
+
+export const updateData = functions.region('europe-west2').pubsub.schedule('every 15 minutes').timeZone('europe/london').onRun((context) => {
 
     const req = https.get(url, (resp) => {
         let data: string = '';
@@ -76,11 +131,10 @@ export const refreshData = functions.https.onRequest((request, response) => {
 
             if (!!lastGrabbed) {
                 const result = storeUpdate(o, 'quick check')
-                response.send(result);
+                console.log(result);
             }
             else {
-                admin.firestore()
-                    .collection('tracking')
+                db.collection('tracking')
                     .orderBy('Updated', 'desc').limit(1).get()
                     .then(querySnapshot => {
                         querySnapshot.forEach(documentSnapshot => {
@@ -89,18 +143,17 @@ export const refreshData = functions.https.onRequest((request, response) => {
                     })
                     .then(() => {
                         const result = storeUpdate(o, 'reload check')
-                        response.send(result);
+                        console.log(result);
                     })
-                    .catch(err => response.send(err));
+                    .catch(err => console.log("Error: " + err.message));
             }
         });
 
     }).on("error", (err) => { console.log("Error: " + err.message); });
 
-
 });
 
-export const graphData = functions.https.onRequest((request, response) => {
+export const graphData = functions.region('europe-west2').https.onRequest((request, response) => {
     response.set('Access-Control-Allow-Origin', '*');
 
     if (!!request.query.clearResults) {
@@ -110,7 +163,7 @@ export const graphData = functions.https.onRequest((request, response) => {
     if (!results || results.cacheUntil < moment()) {
         results = { cacheUntil: moment().add(30, 'm') };
 
-        admin.firestore().collection('tracking').orderBy('Updated', 'desc').get().then(querySnapshot => {
+        db.collection('tracking').orderBy('Updated', 'desc').get().then(querySnapshot => {
             querySnapshot.forEach(doc => {
                 const data = doc.data();
                 for (const key in data) {
@@ -131,7 +184,29 @@ export const graphData = functions.https.onRequest((request, response) => {
     }
 });
 
-export const forceUpdate = functions.https.onRequest((request, response) => {
-    results = null;
-    response.send(`Ok: ${request.query.clearResults}`);
+export const latestData = functions.region('europe-west2').https.onRequest((request, response) => {
+    response.set('Access-Control-Allow-Origin', '*');
+
+    if (!!request.query.clearResults) {
+        latest = null;
+    }
+
+    if (!latest || latest.cacheUntil < moment()) {
+        db.collection('tracking')
+        .orderBy('Updated', 'desc').limit(1).get()
+        .then(querySnapshot => {
+            querySnapshot.forEach(documentSnapshot => {
+                latest = documentSnapshot.data();
+                latest['cacheUntil'] = moment().add(30, 'm');
+            });
+        })
+        .then(() => {
+            response.send(latest);
+        })
+        .catch(err => response.send(err));
+    }
+    else {
+        response.send(latest);
+    }
 });
+ 
