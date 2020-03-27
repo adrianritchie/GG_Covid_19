4949406959f90320168c6c59422b19f92e0f15c3
Adrian RItchie <ar@kodo.gg>
2020-03-27 23:27:26 +0000

    Removed old SPA web app and renamed angular app

--- public/src/app/app.component.spec.ts
+++ public/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'public-ng'`, () => {
+  it(`should have as title 'public'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('public-ng');
+    expect(app.title).toEqual('public');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to public-ng!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to public!');
   });
 });
